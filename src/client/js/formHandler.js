a57981c6b4442a004358c0a69c10e5979121db28
Aither-DB <122840576+Aither-DB@users.noreply.github.com>
2023-04-28 15:11:45 +0930

    Added Weatherbit API and Pixabay API but commented out Pixabay waiting on approval

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,6 +4,10 @@ async function handleSubmit(event) {
 
     const formLocation = document.getElementById('form__location').value;
     const formDeparture = document.getElementById('form__depart').value;
+    const travelDate = new Date(formDeparture);
+    const currentDate = new Date();
+    const daysUntilTrip = Math.ceil((travelDate.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
+   
 
     try {
         const response = await fetch(`http://localhost:8081/data`, {
@@ -16,6 +20,19 @@ async function handleSubmit(event) {
         if (!response.ok) {
             throw new Error('Something went wrong. Please try again later.');
         }
+
+        const data = await response.json();
+
+        console.log(`The latitude of ${formLocation} is ${data.lat}`);
+        console.log(`The longitude of ${formLocation} is ${data.lng}`);
+        console.log(`The country of ${formLocation} is ${data.country}`);
+        console.log(`The date you depart is ${formDeparture}`);
+        console.log(`You depart in ${daysUntilTrip} days`);
+        console.log(`The temperature in ${formLocation} on ${travelDate} will be ${data.temperature} degrees Fahrenheit`);
+        console.log(`The weather in ${formLocation} on ${travelDate} will be ${data.description}`);
+
+
+
     } catch (error) {
         console.log(error);
         alert('Something went wrong. Please try again later.');
--- src/server/index.js
+++ src/server/index.js
@@ -45,28 +45,69 @@ app.post('/data', async (req, res) => {
         }
 
         // Make a request to the GeoNames API to get information about the location
-        const response = await fetch(`http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${process.env.GEONAMES_USERNAME}`);
+        const responseGeoNames = await fetch(`http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${process.env.GEONAMES_USERNAME}`);
 
-        // Parse the response as JSON
-        const data = await response.json();
+        // Parse the responses as JSON
+        const dataGeoNames = await responseGeoNames.json();
 
         // Check if any results were returned
-        if (data.geonames && data.geonames.length > 0) {
+        if (dataGeoNames.geonames && dataGeoNames.geonames.length > 0) {
             // Extract the latitude, longitude, and country from the response
-            const lat = data.geonames[0].lat;
-            const lng = data.geonames[0].lng;
-            const country = data.geonames[0].countryName;
+            const lat = dataGeoNames.geonames[0].lat;
+            const lng = dataGeoNames.geonames[0].lng;
+            const country = dataGeoNames.geonames[0].countryName;
 
             // Log the results to the console
             console.log(`The latitude of ${location} is ${lat}`);
             console.log(`The longitude of ${location} is ${lng}`);
             console.log(`The country of ${location} is ${country}`);
+            console.log(`The date you depart is ${departure}`);
+
+            // Make a request to the Weatherbit API to get weather information for the location and departure date
+            const responseWeatherbit = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&start_date=${departure}&days=1&units=I&key=${process.env.WEATHERBIT_API_KEY}`);
+
+            // Parse the Weatherbit response as JSON
+            const dataWeatherbit = await responseWeatherbit.json();
+
+            // Extract the temperature and weather description from the Weatherbit response
+            const temperature = dataWeatherbit.data[0].temp;
+            const description = dataWeatherbit.data[0].weather.description;
+
+            // Log the results to the console
+            console.log(`The temperature in ${location} on ${departure} will be ${temperature} degrees Fahrenheit`);
+            console.log(`The weather in ${location} on ${departure} will be ${description}`);
+
+            // // Make a request to the Pixabay API to get images for the location
+            // const responsePixabay = await fetch(`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${encodeURIComponent(location)}&image_type=photo&orientation=horizontal&category=places`);
+
+            // // Parse the Pixabay response as JSON
+            // const dataPixabay = await responsePixabay.json();
+
+            // // Check if any results were returned
+            // if (dataPixabay.hits && dataPixabay.hits.length > 0) {
+            //     // Extract the image URL and preview URL from the Pixabay response
+            //     const imageUrl = dataPixabay.hits[0].largeImageURL;
+            //     const previewUrl = dataPixabay.hits[0].previewURL;
+
+            //     // Log the results to the console
+            //     console.log(`The image URL for ${location} is ${imageUrl}`);
+            //     console.log(`The preview URL for ${location} is ${previewUrl}`);
+            // } else {
+            //     console.log(`No images found for ${location}`);
+                
+            //     // Send a message back to the client indicating that no images were found
+            //     res.send(`No images found for ${location}`);
+            // }
 
             // Send the latitude, longitude, and country back to the client
             res.send({
                 lat,
                 lng,
-                country
+                country,
+                temperature,
+                description,
+                // imageURL,
+                // previewURL
             });
         } else {
             console.log(`No results found for ${location}`);
