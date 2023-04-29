87b9bdb697a2d3d507e82fcb10493a7315548c0e
Aither-DB <122840576+Aither-DB@users.noreply.github.com>
2023-04-29 11:05:05 +0930

    Updated with service workers and testing with jest

---
+++ __test__/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler";
+
+describe('handleSubmit', () => {
+  test('should be defined', () => {
+    expect(handleSubmit).toBeDefined();
+  });
+});
